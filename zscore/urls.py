31d749ec7f413411e645a1d1433d8db43323c87b
Gurtej S Kanwar <gurtej@mit.edu>
2013-05-11 18:56:00 -0400

    Adds initial Django project

---
+++ zscore/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'zscore.views.home', name='home'),
+    # url(r'^zscore/', include('zscore.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Admin panel:
+    url(r'^admin/', include(admin.site.urls)),
+)
