5eccab230eab6baef732b4b1c17c27a4d99a33df
Benjamin Kraft <benjaminjkraft@gmail.com>
2013-05-12 03:33:04 -0400

    Adds sleep model

--- zscore/sleep/models.py
+++ zscore/sleep/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Sleep(models.Model):
+    user = models.ForeignKey(User)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    comments = models.TextField()
