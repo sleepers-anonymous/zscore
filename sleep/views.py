9f5fe82e6964eb133b91088d179a78fedc08c819
Gurtej Kanwar <gurtej@mit.edu>
2013-05-15 23:01:29 -0400

    Implements the submit_sleep AJAX view

--- sleep/views.py
+++ sleep/views.py
@@ -1,21 +1,28 @@
 from django.template.loader import render_to_string
 from django.http import HttpResponse
-from datetime import datetime
+from django.contrib.auth.decorators import login_required
+
+from sleep.models import Sleep
+
+import datetime
 
 def home(request):
     return HttpResponse(render_to_string('index.html'))
 
+@login_required
 def mysleep(request):
     return HttpResponse(render_to_string('mysleep.html'))
 
 def leaderboard(request):
     return HttpResponse(render_to_string('leaderboard.html'))
 
+@login_required
 def submitSleep(request):
-    start = datetime(*(map(int, request.POST.getlist("start[]"))))
-    end = datetime(*(map(int, request.POST.getlist("end[]"))))
-    center = datetime(*(map(int, request.POST.getlist("center[]"))))
-    print "start", start
-    print "end", end
-    print "center", center
+    # TODO: Accept comments
+    # Date-ify start, end, and center
+    start = datetime.datetime(*(map(int, request.POST.getlist("start[]"))))
+    end = datetime.datetime(*(map(int, request.POST.getlist("end[]"))))
+    center = datetime.date(*(map(int, request.POST.getlist("center[]"))[:3]))
+    # Create the Sleep instance
+    Sleep.objects.create(user=request.user, start_time=start, end_time=end, comments="", date=center)
     return HttpResponse('')
