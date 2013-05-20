b34ae7a0580742945f2ffed8ff37b5bfedecfeb2
Benjamin Kraft <benjaminjkraft@gmail.com>
2013-05-20 02:21:26 -0400

    Add path stuff to wsgi

--- zscore/wsgi.py
+++ zscore/wsgi.py
@@ -13,7 +13,15 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+import os,sys
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
+
+path = '/usr/share/myapplication'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zscore.settings")
 
