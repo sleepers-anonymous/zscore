18f5e7d602b82e6ee75037ca13651858d6f8d858
Benjamin Kraft <benjaminjkraft@gmail.com>
2013-05-22 13:43:31 -0400

    Add STATIC_ROOT to settings.py

--- zscore/settings.py
+++ zscore/settings.py
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PWD, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
