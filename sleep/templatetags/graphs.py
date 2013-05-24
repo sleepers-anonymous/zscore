1c0f4c9393bc978bb05c7bdc9c3842bc2f6bf8c1
Benjamin Kraft <benjaminjkraft@gmail.com>
2013-05-24 18:18:40 -0400

    Adds graph with time of day bars

--- sleep/templatetags/graphs.py
+++ sleep/templatetags/graphs.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.timezone import localtime
 from sleep.models import *
 import datetime
 register = template.Library()
@@ -7,3 +8,41 @@ register = template.Library()
 def graphPerDay(user):
     sleeper = Sleeper.objects.get(pk=user.pk)
     return { 'graphData' : sleeper.sleepPerDay(packDates=True,hours=True) }
+
+@register.inclusion_tag('inclusion/graph_time_of_day_bars.html')
+def graphTimeOfDayBars(user):
+    sleeper = Sleeper.objects.get(pk=user.pk)
+    sleeps = sleeper.sleep_set.all()
+    first = min([localtime(s.start_time) for s in sleeps]).date()
+    last = max([localtime(s.end_time) for s in sleeps]).date()
+    n = (last-first).days + 1
+    dateRange = [first + datetime.timedelta(i) for i in range(n)]
+    for i in range(n):
+        dateRange[i] = (i,dateRange[i])
+    times = range(25)
+    height = n*15+15
+    
+    sleepsProcessed = []
+    for sleep in sleeps:
+        startDate = localtime(sleep.start_time).date()
+        endDate = localtime(sleep.end_time).date()
+        dr = [startDate + datetime.timedelta(i) for i in range((endDate-startDate).days + 1)]
+        for d in dr:
+            if d == startDate:
+                startTime = localtime(sleep.start_time).time()
+            else:
+                startTime = datetime.time(0)
+            if d == endDate:
+                endTime = localtime(sleep.end_time).time()
+            else:
+                endTime = datetime.time(23,59)
+            sleepsProcessed.append((startTime.hour * 15 + startTime.minute / 4., (d-first).days * 15, endTime.hour * 15 + endTime.minute / 4. - startTime.hour * 15 - startTime.minute / 4., 15))
+
+    context = {
+            'sleeps' : sleepsProcessed,
+            'dateRange' : dateRange,
+            'times' : times,
+            'height' : height,
+            }
+    return context
+    
