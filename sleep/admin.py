551ecdfa593b4baae3013d35f97731c6d36c15e9
Benjamin Kraft <benjaminjkraft@gmail.com>
2013-06-05 17:14:34 -0400

    Adds stuff to admin.

--- sleep/admin.py
+++ sleep/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from sleep.models import *
 
+admin.site.register(AllNighter)
 admin.site.register(Sleep)
 admin.site.register(SleeperProfile)
+admin.site.register(FriendRequest)
