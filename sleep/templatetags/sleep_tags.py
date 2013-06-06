f8e0a266d6077c430c9ce68191509a85f706c85e
Benjamin Kraft <benjaminjkraft@gmail.com>
2013-06-06 12:05:06 -0400

    Another syntax error.

--- sleep/templatetags/sleep_tags.py
+++ sleep/templatetags/sleep_tags.py
@@ -16,7 +16,7 @@ def sleepStatsView(context, renderContent='html'):
     context['lastDay'] = sleeper.timeSleptByTime(now-datetime.timedelta(1),now)
     context['total'] = sleeper.timeSleptByDate()
     context['renderContent'] = renderContent
-return context
+    return context
 
 @register.inclusion_tag('inclusion/stats_table.html')
 def sleepStatsTable(user):
