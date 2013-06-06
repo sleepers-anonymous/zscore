fd36d1730bdc4bf2c92f312422e1f386c8383c03
shulinye <nightshadequeen@gmail.com>
2013-06-06 14:34:01 -0400

    Slight changes to sleep edit/entry validation

--- sleep/views.py
+++ sleep/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.core import serializers
 from django.db.models import Q
-from django.core.exceptions import ValidationError, NON_FIELD_ERROR
+from django.core.exceptions import ValidationError
 
 from sleep.models import *
 from sleep.forms import *
@@ -53,7 +53,7 @@ def editOrCreateSleep(request,sleep = None):
             if create: return HttpResponse(render_to_string('simplecreation.html', context, context_instance=RequestContext(request)))
             context.update({ "start": sleep[0].start_time.strftime(tformat), "end": sleep[0].end_time.strftime(tformat)})
             return HttpResponse(render_to_string('editsleep.html',context,context_instance=RequestContext(request)))
-        except ValidationError:
+        except ValidationError, e:
             if "forceOverlap" in request.POST and request.POST['forceOverlap'] == 'on':
                 if create:
                     newsleep = form.save(commit=False)
@@ -66,7 +66,7 @@ def editOrCreateSleep(request,sleep = None):
                 context["successfulSave"] = True
             else:
                 context["successfulSave"] = False
-                context["saveError"] = "overlapping"
+                context["saveError"] = e
             context["form"] = form
             if create: return HttpResponse(render_to_string('simplecreation.html', context, context_instance=RequestContext(request)))
             context.update({ "start": sleep[0].start_time.strftime(tformat), "end": sleep[0].end_time.strftime(tformat)})
