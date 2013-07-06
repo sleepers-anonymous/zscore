377f4b709607b988b1157ea9475e34c19e51e37b
Benjamin Kraft <benjaminjkraft@gmail.com>
2013-07-06 01:30:02 -0400

    Fixes various small bugs in datetime util tags.

--- sleep/templatetags/datetime_utils.py
+++ sleep/templatetags/datetime_utils.py
@@ -4,33 +4,63 @@ register = template.Library()
 
 @register.filter(name='printHHMM')
 def printHHMM(value):
-    '''takes a datetime and prints it as HH:MM'''
-    return ':'.join(str(value).split(':')[:2])
+    '''takes a datetime.timedelta and prints it as HH:MM'''
+    s=int(value.total_seconds())
+    if s<0: #so we round towards zero rather than towards -Infinity
+        sign="-"
+        s=-s
+    else:
+        sign=""
+    h,s=divmod(s,3600)
+    m,s=divmod(s,60)
+    return "%s%d:%02d" % (sign,h,m)
+
+def print DHHMM(value):
+    '''takes a datetime.timedelta and prints it as D days, HH:MM'''
+    s=int(value.total_seconds())
+    if s<0: #so we round towards zero rather than towards -Infinity
+        sign="-"
+        s=-s
+    else:
+        sign=""
+    d,s=divmod(s,86400)
+    h,s=divmod(s,3600)
+    m,s=divmod(s,60)
+    if d:
+        return "%s%d days, %d:%02d" % (sign,d,h,m)
+    return "%s:%02d" % (sign,h,m)
 
 @register.filter(name='printDHM')
 def printDHM(value):
-    '''takes a datetime and prints it as HH:MM'''
-    parts=[]
-    if ',' in str(value):
-        d,t=str(value).split(',')
-        parts.append(d)
+    '''takes a datetime.timedelta and prints it as D day(s), H hour(s), and M minute(s)'''
+    s=int(value.total_seconds())
+    if s<0: #so we round towards zero rather than towards -Infinity
+        sign="negative "
+        s=-s
     else:
-        t=str(value)
-    h,m,s=t.split(':')
-    if int(h)==1:
+        sign=""
+    d,s=divmod(s,86400)
+    h,s=divmod(s,3600)
+    m,s=divmod(s,60)
+    parts=[]
+    if d==1:
+        parts.append("1 day")
+    elif d>1:
+        parts.append(str(d)+" days")
+    if h==1:
         parts.append("1 hour")
-    elif int(h)>1:
-        parts.append(h.strip()+" hours")
-    if int(m)==1:
+    elif h>1:
+        parts.append(str(h)+" hours")
+    if m==1:
         parts.append("1 minute")
-    elif int(m)>1:
-        parts.append(m.strip()+" minutes")
+    elif m>1:
+        parts.append(str(m)+" minutes")
     if len(parts)==0:
         return "none"
     elif len(parts)==1:
-        return parts[0]
+        return sign+parts[0]
     elif len(parts)==2:
-        return parts[0]+" and "+parts[1]
+        return sign+parts[0]+" and "+parts[1]
     elif len(parts)==3:
-        return parts[0]+", "+parts[1]+", and "+parts[2]
+        return sign+parts[0]+", "+parts[1]+", and "+parts[2]
 
