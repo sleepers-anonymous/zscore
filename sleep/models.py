530cd25f0daeeed0ecde4d436dc7bf366a6d2d30
shulinye <nightshadequeen@gmail.com>
2013-08-04 19:28:06 -0400

    Helloworld, I'm an idiot!

--- sleep/models.py
+++ sleep/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import *
 from django.utils.timezone import now
-from django.core.mail import send_mail()
+from django.core.mail import send_mail
 
 import pytz
 import datetime
