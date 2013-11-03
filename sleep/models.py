55ca975db78ef2a763b3f60d276eaa61af391e12
Benjamin Kraft <benjaminjkraft@gmail.com>
2013-11-03 18:18:54 -0500

    Fixes fix to timezone display.

--- sleep/models.py
+++ sleep/models.py
@@ -247,7 +247,7 @@ class Sleep(models.Model):
 
     def getTZShortName(self):
         """Gets the short of a time zone"""
-        return self.getSleepTZ().tzname(self.end_time)
+        return self.getSleepTZ().tzname(self.end_local_time())
 
     def save(self, *args, **kwargs):
         seconds = self.length().total_seconds()
