ac5e6e0b77db4ea509c63c7e5f7232c9dc60eac9
root <root@zscore.mit.edu>
2013-11-19 16:40:26 -0500

    Makes flock exist every hour to check if the zephyrbot is still up

--- useful_scripts/run.sh
+++ useful_scripts/run.sh
@@ -2,4 +2,8 @@
 export PYTHONIOENCODING=utf8
 k5start -v -f /etc/krb5.keytab daemon/zscore.mit.edu
 
-flock /tmp/ -c /zscore/useful_scripts/zephyrbot.py
+while :
+do
+    flock /tmp/ -c /zscore/useful_scripts/zephyrbot.py
+    sleep 3600
+done
