45799314502f6182517081ac24faf7e82958539f
root <root@zscore.mit.edu>
2013-11-19 16:35:03 -0500

    Makes it actually run

--- useful_scripts/run.sh
+++ useful_scripts/run.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 export PYTHONIOENCODING=utf8
-#k5start -v -f [KEYTAB] daemon/zscore.mit.edu
+k5start -v -f /etc/krb5.keytab daemon/zscore.mit.edu
 
+flock /tmp/ -c /zscore/useful_scripts/zephyrbot.py
