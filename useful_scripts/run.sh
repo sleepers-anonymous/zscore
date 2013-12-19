db7157a688c3e804d6ef4fb5947ec0dfce12182a
Shulin Ye <nightshadequeen@gmail.com>
2013-12-18 21:01:20 -0500

    Fixes the flock

--- useful_scripts/run.sh
+++ useful_scripts/run.sh
@@ -4,6 +4,6 @@ k5start -v -f /etc/krb5.keytab daemon/zscore.mit.edu
 
 while :
 do
-    flock /tmp/ -c /zscore/useful_scripts/zephyrbot.py
+    flock -n /tmp/ -c /zscore/useful_scripts/zephyrbot.py
     sleep 3600
 done
