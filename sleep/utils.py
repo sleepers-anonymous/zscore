78a5b4e7acf9a1436b55bfbde538644918c42ea6
Shulin Ye <nightshadequeen@gmail.com>
2014-02-17 14:41:12 -0500

    According to lua, my python was overly verbose.

--- sleep/utils.py
+++ sleep/utils.py
@@ -21,8 +21,8 @@ def zephyrDisplay(stats, usermetrics = None):
     if usermetrics == None:
         usermetrics = stats.keys()
     else:
-        usermetrics = set([str(m) for m in usermetrics]).intersection(stats.keys())
-    return '\n'.join([(i + ": ").rjust(15) + str(stats[i]) for i in usermetrics])
+        usermetrics = set([str(m) for m in usermetrics]) & stats.viewkeys()
+    return '\n'.join((i + ": ").rjust(15) + str(stats[i]) for i in usermetrics)
 
 #------------------------------Regexes for mobile detection ------------------------------
 userAgentsTestMatch = r'^(?:%s)' % '|'.join((
