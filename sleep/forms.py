777ff4cb8ff4e90b48b8e8437f93c11b73419cb6
Shulin Ye <nightshadequeen@gmail.com>
2014-06-21 16:24:13 -0400

    Switches metrics-to-display to Checkbox

--- sleep/forms.py
+++ sleep/forms.py
@@ -1,9 +1,25 @@
 from django import forms
 from sleep.models import *
 from django.core.exceptions import *
+from django.utils.safestring import mark_safe
 
 import pytz
 import datetime
+
+#from https://stackoverflow.com/questions/7852609/django-setting-class-of-ul-instead-of-input-with-checkboxselectmultiple-widg
+class CheckboxSelectMultipleULAttrs(forms.CheckboxSelectMultiple):
+    """
+    Class to allow setting attributes on containing ul in a CheckboxSelectMultiple
+    """
+    def __init__(self, ulattrs=None, attrs=None, choices=()):
+        self.ulattrs = ulattrs
+        super(CheckboxSelectMultipleULAttrs, self).__init__(attrs, choices)
+
+    def render(self, name, value, attrs=None, choices=()):
+        html = super(CheckboxSelectMultipleULAttrs, self).render(name, value, attrs, choices)
+        if not self.ulattrs: return html
+        ulattributes = " ".join(i + "='" + self.ulattrs[i] + "'" for i in self.ulattrs)
+        return mark_safe(html.replace('<ul', '<ul ' + ulattributes))
 
 class GroupForm(forms.ModelForm):
     delete = forms.BooleanField(required=False)
@@ -23,6 +39,8 @@ class SleeperProfileForm(forms.ModelForm):
         model = SleeperProfile
         fields = ['privacy','privacyLoggedIn','privacyFriends', 'partyMode', 'use12HourTime', 'idealSleep', 'timezone', 'useGravatar', 'autoAcceptGroups',
                 'idealWakeupWeekend', 'idealWakeupWeekday', 'idealSleepTimeWeekday', 'idealSleepTimeWeekend', 'mobile', 'punchInDelay', 'metrics']
+
+        widgets = {'metrics': CheckboxSelectMultipleULAttrs(ulattrs={'class':'checkboxselectmultiple'})}
 
     def __init__(self, fmt, *args, **kwargs):
         self.fmt = fmt
