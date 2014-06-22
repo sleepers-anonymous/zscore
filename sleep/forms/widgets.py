b3f518e5f3656001d00e26ceef02c9b2c71a1561
Shulin Ye <nightshadequeen@gmail.com>
2014-06-22 15:53:41 -0400

    Minor bugfix

--- sleep/forms/widgets.py
+++ sleep/forms/widgets.py
@@ -17,6 +17,6 @@ class CheckboxSelectMultipleULAttrs(forms.CheckboxSelectMultiple):
     def render(self, name, value, attrs=None, choices=()):
         html = super(CheckboxSelectMultipleULAttrs, self).render(name, value, attrs, choices)
         if not self.ulattrs: return html
-        ulattributes = " ".join(i + "='" + self.ulattrs[i] + "'" for i in self.ulattrs)
+        ulattributes = " ".join(i + "='" + self.ulattrs[i] + "' " for i in self.ulattrs)
         return mark_safe(html.replace('<ul', '<ul ' + ulattributes, 1))
 
