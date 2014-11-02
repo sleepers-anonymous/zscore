d57acb530e5b7fc64eaebb93b05b9a6a40483df6
Ben Kraft <benjaminjkraft@gmail.com>
2014-11-02 14:42:24 -0500

    Add dropbox_dbdump.sh from the server

---
+++ useful_scripts/dropbox_dbdump.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+date=`date +"%d-%m-%Y"`
+# Output to dropbox in archive format
+pg_dump -f ${HOME}/Dropbox/dbdump/zscore_db_${date}.sql -F tar
+# Check that dropbox is running
+pgrep dropbox || ${HOME}/.dropbox-dist/dropboxd
