b0b911825f186299001f23050f472265d3da3111
Ben Kraft <benjaminjkraft@gmail.com>
2015-06-11 22:02:52 -0400

    Quiet pgrep in dbdump script

--- useful_scripts/dropbox_dbdump.sh
+++ useful_scripts/dropbox_dbdump.sh
@@ -4,4 +4,4 @@ date=`date +"%d-%m-%Y"`
 # Output to dropbox in archive format
 pg_dump -f ${HOME}/Dropbox/dbdump/zscore_db_${date}.sql -F tar
 # Check that dropbox is running
-pgrep dropbox || ${HOME}/.dropbox-dist/dropboxd
+pgrep dropbox >/dev/null || ${HOME}/.dropbox-dist/dropboxd
