6b124727a1141bbc68cd7da5b723dcfcc4ff5978
Ben Kraft <benjaminjkraft@gmail.com>
2016-05-19 00:27:27 -0700

    Remove django.contrib.sites

--- zscore/settings.py
+++ zscore/settings.py
@@ -54,8 +54,6 @@ TIME_ZONE = 'America/New_York'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
-
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
@@ -127,7 +125,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
