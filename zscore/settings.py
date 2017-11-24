a17b210f3a574a4011a1826e2a6cf3c1e93262dc
Ben Kraft <ben@benkraft.org>
2017-11-24 12:43:19 -0500

    Ignore django logging and just use appengine's

--- zscore/settings.py
+++ zscore/settings.py
@@ -155,40 +155,8 @@ if not DEBUG:
         }
     }
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'console': {
-            'level': 'INFO',
-            'filters': [],
-            'class': 'logging.StreamHandler'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['mail_admins', 'console'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-    },
-}
-
+# App Engine's logging is better so we just use that.
+LOGGING_CONFIG = None
 
 SERVER_EMAIL = 'noreply@zscoresleep.appspotmail.com'
 if DEBUG:
